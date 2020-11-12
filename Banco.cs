05ea21c10aa2a571a486a01750337a3d97c15b00
matheofelippe <69490109+matheofelippe@users.noreply.github.com>
2020-11-11 23:56:20 -0300

    Codigos

---
+++ Banco.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exercício_individual___Tema_Banco
+{
+    class Banco
+    {
+        public int numConta;
+        public string titularConta;
+        public string depositoInicial;
+        public float saldo = 0;
+        public float deposito;
+        public float saque;
+        public float taxa = 5;
+
+        public override string ToString()
+        {
+            return "DADOS DA CONTA: " + "CONTA " + numConta + ", TÍTULAR: " + titularConta + ", SALDO R$" + saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void AdcionaSaldo(float Deposito)
+        {
+            saldo = saldo + Deposito;
+        }
+
+        public void RealizarSaque(float Saque)
+        {
+            saldo = saldo - taxa;
+            saldo = saldo - Saque;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercício_individual___Tema_Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco conta = new Banco();
+
+            Console.Write("ENTRE COM O NÚMERO DA CONTA: ");
+            conta.numConta = int.Parse(Console.ReadLine());
+
+            Console.Write("ENTRE COM O TITULAR DA CONTA: "); //Digitar decimais com virgula, NÃO ponto.
+            conta.titularConta = Console.ReadLine();
+
+            Console.Write("HAVERÁ DEPOSITO INICIAL (S/N): ");
+            conta.depositoInicial = Console.ReadLine();
+
+            if(conta.depositoInicial == "S") //usar APENAS S ou N maisculo
+            {
+                Console.Write("ENTRE O VALOR DE DEPÓSITO INICIAL: ");
+                conta.saldo = float.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("ENTRE COM UM VALOR PARA DEPÓSITO: "); //Digitar decimais com virgula, NÃO ponto.
+            conta.deposito = float.Parse(Console.ReadLine());
+            conta.AdcionaSaldo(conta.deposito);
+
+            Console.WriteLine();
+            Console.WriteLine("DADOS ATUALIZADOS: " + conta);
+
+            Console.WriteLine();
+            Console.Write("ENTRE COM UM VALOR PARA SAQUE: "); //Digitar decimais com virgula, NÃO ponto.
+            conta.saque = float.Parse(Console.ReadLine());
+            conta.RealizarSaque(conta.saque);
+
+            Console.WriteLine();
+            Console.WriteLine("DADOS ATUALIZADOS: " + conta);
+
+            Console.ReadKey();
+        }
+    }
+}
